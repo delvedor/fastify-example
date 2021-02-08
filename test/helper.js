e928acd7eef5b5876ff3a63d1ea57f776f11ba1e
Tomas Della Vedova <delvedor@users.noreply.github.com>
2021-02-08 08:51:30 +0100

    Fix CI test (#4)

--- test/helper.js
+++ test/helper.js
@@ -1,15 +1,26 @@
 import Fastify from 'fastify'
 import fp from 'fastify-plugin'
-import { join } from 'desm'
-import dotenv from 'dotenv'
 import Mock from '@elastic/elasticsearch-mock'
 import App from '../app.js'
 
 // We need to load the env configuration
-dotenv.config({ path: join(import.meta.url, '..', '.env') })
+process.env.NODE_ENV = 'development'
+process.env.ELASTIC_ADDRESS = 'http://localhost:9200'
+process.env.ELASTIC_API_KEY = '<api-key>'
+process.env.GITHUB_APP_ID = '<app-id>'
+process.env.GITHUB_APP_SECRET = '<app-secret>'
+process.env.COOKIE_SECRET = '<cookie-secret>'
+process.env.ALLOWED_USERS = 'foo@bar.com'
 
 export async function build (t, opts = {}) {
   const app = Fastify()
+  // Currently we are always relying on the Mock being present
+  // in our tests, an improvement would be to test against a local
+  // living instance of Elasticsearch, but for the sake of simplicity
+  // of the demo we will always use a mock.
+  if (!opts.elasticMock) {
+    opts.elasticMock = buildElasticMock()
+  }
   await app.register(fp(App), { testing: true, ...opts })
 
   t.tearDown(app.close.bind(app))
